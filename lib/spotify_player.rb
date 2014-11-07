443aa0adc48718af4c0e0c01f853d3bdf0a1c501
Jamie Patel <jamie@notespublication.com>
2014-11-07 16:57:14 +0000

    Playlist/play queue working

--- lib/spotify_player.rb
+++ lib/spotify_player.rb
@@ -1,11 +1,23 @@
 require 'spotify-client'
+require 'securerandom'
 
 module PlaySpotify
 
+	def client 
+		@client ||= Spotify::Client.new access_token: 'BQCbrvp9QCtpalIG31pLdCpsWKlwsLq_5cVU4JJdzVM16vqKzI45UIQCs9HNKT83yoheK9XsS3IVlYy-oA9Laf4v4j5HLnin7JHv7amfeWJuv5JfR9bemGhkMZmg6jku5MHaC9xH8EzMXvm_ljaQyG4stThodqt-v3LLExomRAw'
+	end
+
 	def track name
-		client = Spotify::Client.new
 		@item = client.search("track", name).first[1]["items"].first
 		@item["uri"]
+	end
+
+	def create_playlist
+		@playlist = client.create_user_playlist 'jpatel1', SecureRandom.hex
+	end
+
+	def add_to_playlist name
+		client.add_user_tracks_to_playlist 'jpatel1', @playlist["id"], [track(name)]
 	end
 
 	def play song
--- plugins/hip_spot.rb
+++ plugins/hip_spot.rb
@@ -1,6 +1,7 @@
 require_relative '../lib/spotify_player'
 require 'httparty'
 require 'yaml'
+require 'rufus-scheduler'
 
 class Robut::Plugin::HipSpot
 
@@ -14,9 +15,21 @@ class Robut::Plugin::HipSpot
   	match /^!play (.*)/ do |query|
   		# message = HTTParty.post(URL, body: {song: query}.to_json, headers: { 'Content-Type' => 'application/json' })
   		# reply JSON.parse(message)["message"]
+  		store['track'] = query
 
-  		play track query
-  		reply playing_message
+  		player_state = `osascript -e 'tell application "Spotify" to player state'`.chomp
+  		if player_state != 'playing'
+	  		# play track query
+	  		# reply playing_message
+	  		create_playlist
+	  		add_to_playlist query
+	  		play @playlist["uri"]
+	  		store['current_playlist'] = @playlist
+  		else
+  			create_playlist unless store['current_playlist']
+  			store['current_playlist'] ? (@playlist = store['current_playlist']) : create_playlist
+  			add_to_playlist query
+  		end
 
   	end
 
