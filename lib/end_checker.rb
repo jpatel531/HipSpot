9f877403b163258e1ac790445833c8193f9ed060
Jamie Patel <jamie@notespublication.com>
2015-02-22 14:15:59 +0000

    End_checker checks for the end of the playlist and writes it to yaml

--- lib/end_checker.rb
+++ lib/end_checker.rb
@@ -1,15 +1,40 @@
 # end_checker = Thread.new do
 
-# 	loop do 
+require_relative 'base'
 
-# 		playlist = Playlist.from_store(store)
-# 		@playing_last_song = (SpotifyController.current_song == playlist.last_song)
+def store_beyond_last_song(store, beyond_last_song)
+	store['beyond_last_song'] = beyond_last_song
+	File.open('.robut', 'w') {|f| f.write store.to_yaml } #Store
+end
 
-# 		playlist_completed = @playing_last_song && (SpotifyController.player_state == 'stopped') 
+loop do 
+	begin
 
-# 		sleep 1
+		current_song = SpotifyController.current_song
+		next if current_song == @current_song
 
-# 	end
+		store = YAML.load(File.read('.robut'))
+		id = store['current_playlist']
+		playlist = Playlist.from_id(id)
+
+		@current_song = current_song
+
+		beyond_last_song = @playing_last_song && (@current_song == playlist.first_song) 
+
+		store_beyond_last_song(store, beyond_last_song)
+
+		puts "Playlist completed? #{beyond_last_song}"
+
+		break if beyond_last_song
+
+		@playing_last_song = (SpotifyController.current_song == playlist.last_song)
+
+		puts "Playing last song? #{@playing_last_song}"
 
 
-# end
+		sleep 1
+
+	rescue
+		next
+	end
+end
